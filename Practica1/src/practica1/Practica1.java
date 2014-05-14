3522bf429f780af2826037132079478c0c4d8ce4
joviedocorella <joviedocorella@gmail.com>
2014-05-13 20:47:08 -0600

    This is the first practice

---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,191 @@
+/*
+    *Name: Class2
+    
+    *Information:This is the second class 
+    
+    *Date:13/5/2014
+    
+    *Copyright
+
+ */
+
+package practica1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jose Oviedo
+ */
+public class Practica1 {
+
+    /*
+     * @param args the command line arguments
+     * This is the main method
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        /*
+        *Exercise #1
+        */
+        /*
+        int N;
+        double A;
+        char C;
+        System.out.println("Digite el valor int que le quere asignar a la variable N");
+        N = Integer.parseInt(teclado.nextLine());
+        System.out.println("Digite el valor double que le quere asignar a la variable A");
+        A = Double.parseDouble(teclado.nextLine());
+        System.out.println("Digite el valor char que le quere asignar a la variable C");
+        C = (teclado.nextLine()).charAt(0);
+        System.out.println("La variable N tiene el valor de: "+N);
+        System.out.println("La variable A tiene el valor de: "+A);
+        System.out.println("La variable C tiene el valor de: "+C);
+        System.out.println("El resultado de la suma N + A es de: "+(N+A));
+        System.out.println("El resultado de la diferencia A - N es de: "+(A-N));
+        System.out.println("El valor numerico del caracter contenido en la variable C es de: "+ (int)C);
+        */
+        
+        
+        /*
+        *Exercise #2
+        */
+        /*
+        int X,Y;
+        double N, M;
+        System.out.println("Digite el valor int que le quere asignar a la variable X");
+        X = Integer.parseInt(teclado.nextLine());
+        System.out.println("Digite el valor int que le quere asignar a la variable Y");
+        Y = Integer.parseInt(teclado.nextLine());
+        System.out.println("Digite el valor double que le quere asignar a la variable N");
+        N = Double.parseDouble(teclado.nextLine());
+        System.out.println("Digite el valor double que le quere asignar a la variable M");
+        M = Double.parseDouble(teclado.nextLine());
+        System.out.println("El resultado de la suma N + M es de: "+(N+M));
+        System.out.println("El resultado de la suma X + Y es de: "+(X+Y));
+        System.out.println("El resultado de la resta N - M es de: "+(N-M));
+        System.out.println("El resultado de la resta X - Y es de: "+(X-Y));
+        System.out.println("El resultado de la multiplicacion N * M es de: "+(N*M));
+        System.out.println("El resultado de la multiplicacion X * Y es de: "+(X*Y));
+        System.out.println("El resultado de la division N / M es de: "+(N/M));
+        System.out.println("El resultado de la division X / Y es de: "+(X/Y));
+        */
+       
+        
+        /*
+        *Exercise #3
+        */
+        /*
+        int N;
+        System.out.println("Digite el valor int que le quere asignar a la variable N");
+        N= Integer.parseInt(teclado.nextLine());
+        System.out.println("El resultado de N incrementado en 77 es de: "+(N+77));
+        System.out.println("El resultado de N decrementado en 3 es de: "+(N-3));
+        System.out.println("El resultado de N duplucado es de: "+(N*2));
+        */
+        
+        
+        /*
+        *Exercise #4
+        */
+        /*
+        int A,B,C,D;
+        System.out.println("Digite el valor int que le quere asignar a la variable A");
+        A= Integer.parseInt(teclado.nextLine());
+        System.out.println("Digite el valor int que le quere asignar a la variable B");
+        B= Integer.parseInt(teclado.nextLine());
+        System.out.println("Digite el valor int que le quere asignar a la variable C");
+        C= Integer.parseInt(teclado.nextLine());
+        System.out.println("Digite el valor int que le quere asignar a la variable D");
+        D= Integer.parseInt(teclado.nextLine());
+        B=C;
+        C=A;
+        A=D;
+        D=B;
+        System.out.println("La variable A tiene el valor de: "+A);
+        System.out.println("La variable B tiene el valor de: "+B);
+        System.out.println("La variable C tiene el valor de: "+C);
+        System.out.println("La variable D tiene el valor de: "+D);
+        */
+        
+        
+        /*
+        *Exercise #5
+        */
+        /*
+        int A;
+        System.out.println("Digite el valor int que le quere asignar a la variable A");
+        A= Integer.parseInt(teclado.nextLine());
+        if(A%2==0)
+        {
+            System.out.println("El valor de A es par");
+        } else {
+            System.out.println("El valor de A es impar");
+        }
+        */
+        
+        
+        /*
+        *Exercise #6
+        */
+        /*
+        int B;
+        System.out.println("Digite el valor int que le quere asignar a la variable B");
+        B= Integer.parseInt(teclado.nextLine());
+        if(B>=0){
+            System.out.println("El valor que tiene la variable B es positivo");
+        }
+        else {
+            System.out.println("El valor que tiene la variable B es negativo");
+        }
+        */
+        
+        /*
+        *Exercise #7
+        */
+        /*
+        int C;
+        System.out.println("Digite el valor int que le quere asignar a la variable C");
+        C= Integer.parseInt(teclado.nextLine());
+        if(C>=0){
+            System.out.println("El valor que tiene la variable C es positivo");
+        }
+        if(C<0){
+            System.out.println("El valor que tiene la variable C es negativo");
+        }
+        if(C%2==0)
+        {
+            System.out.println("El valor de C es par");
+        }
+        if(C%2!=0)
+        {
+            System.out.println("El valor de C es impar");
+        }
+        if(C%5==0)
+        {
+            System.out.println("El valor de C es multiplo de 5");
+        }
+        if(C%10==0)
+        {
+            System.out.println("El valor de C es multiplo de 10");
+        }
+        if(C%5!=0)
+        {
+            System.out.println("El valor de C no es multiplo de 5");
+        }
+        if(C%10!=0)
+        {
+            System.out.println("El valor de C no es multiplo de 10");
+        }
+        if(C>=100)
+        {
+            System.out.println("El valor de C es mayor que 100");
+        }
+        if(C<100)
+        {
+            System.out.println("El valor de C es menor que 100");
+        }
+        */
+    }
+    
+}
